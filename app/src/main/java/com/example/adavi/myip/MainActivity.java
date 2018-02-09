df736d571484f3d9346905c09998a691c994b373
prasanna189 <spkforwork@gmail.com>
2018-02-09 19:51:02 +0530

    removed all warning and removed Utils.java

--- app/src/main/java/com/example/adavi/myip/MainActivity.java
+++ app/src/main/java/com/example/adavi/myip/MainActivity.java
@@ -4,43 +4,81 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class MainActivity extends AppCompatActivity {
 
-    Utils utils;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("My IP address");
         setContentView(R.layout.activity_main);
-        final TextView tv= (TextView)findViewById(R.id.tv_1);
-        Button bt = (Button)findViewById(R.id.button1);
-        utils = new Utils();
+        final TextView tv= (TextView)findViewById(R.id.ipaddr_tv);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        Button bt = (Button)findViewById(R.id.refresh_button);
+
+        tv.setText(R.string.no_ip);
+
+        String IP;
+        IP = getIP();
+
+        if(IP!=null)
+        {
+            if(IP.length() > 0){
+                tv.setText(IP);
+            }
+        }
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String ipAddress;
-                ipAddress = utils.getIPAddress(true);
-                if(ipAddress.length()>0){
-                    tv.setText(ipAddress);
+
+                //toast to alert user that it will take sometime for device to update its ip address
+                Toast.makeText(MainActivity.this, "It may take some time for the device to update IP address", Toast.LENGTH_LONG).show();
+                tv.setVisibility(View.INVISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+                String IP = getIP();
+                tv.setText(R.string.no_ip);
+                if(IP != null)
+                {
+                    if(IP.length()>0){
+                        tv.setText(IP);
+                    }
                 }
-                else {
-                    tv.setText("No IP available");
-                }
+                tv.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.INVISIBLE);
 
             }
         });
-//        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
-//        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
-        if(utils.getIPAddress(true).length() > 0){
-            tv.setText(utils.getIPAddress(true));
-        }
-        else{
-            tv.setText("No IP available");
-        }
+
+    }
 
 
+
+    public static String getIP() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
