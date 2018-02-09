3b5f9020f609a8303604bc15beab4afa725090dc
prasanna189 <spkforwork@gmail.com>
2018-02-09 20:21:00 +0530

    activated Proguard

--- app/src/main/java/com/example/adavi/myip/MainActivity.java
+++ app/src/main/java/com/example/adavi/myip/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.adavi.myip;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -17,59 +18,11 @@ import java.util.Enumeration;
 public class MainActivity extends AppCompatActivity {
 
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setTitle("My IP address");
-        setContentView(R.layout.activity_main);
-        final TextView tv= (TextView)findViewById(R.id.ipaddr_tv);
-        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        Button bt = (Button)findViewById(R.id.refresh_button);
-
-        tv.setText(R.string.no_ip);
-
-        String IP;
-        IP = getIP();
-
-        if(IP!=null)
-        {
-            if(IP.length() > 0){
-                tv.setText(IP);
-            }
-        }
-
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //toast to alert user that it will take sometime for device to update its ip address
-                Toast.makeText(MainActivity.this, "It may take some time for the device to update IP address", Toast.LENGTH_LONG).show();
-                tv.setVisibility(View.INVISIBLE);
-                progressBar.setVisibility(View.VISIBLE);
-                String IP = getIP();
-                tv.setText(R.string.no_ip);
-                if(IP != null)
-                {
-                    if(IP.length()>0){
-                        tv.setText(IP);
-                    }
-                }
-                tv.setVisibility(View.VISIBLE);
-                progressBar.setVisibility(View.INVISIBLE);
-
-            }
-        });
-
-    }
-
-
-
     public static String getIP() {
         try {
-            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
                 NetworkInterface intf = en.nextElement();
-                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
                     InetAddress inetAddress = enumIpAddr.nextElement();
                     if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
                         return inetAddress.getHostAddress();
@@ -80,5 +33,51 @@ public class MainActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
         return null;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("My IP address");
+        setContentView(R.layout.activity_main);
+        final TextView tv = (TextView) findViewById(R.id.ipaddr_tv);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        Button bt = (Button) findViewById(R.id.refresh_button);
+
+        tv.setText(R.string.no_ip);
+
+        String IP;
+        IP = getIP();
+
+        if (IP != null) {
+            if (IP.length() > 0) {
+                tv.setText(IP);
+            }
+        }
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //toast to alert user that it will take sometime for device to update its ip address
+                Toast toast = Toast.makeText(MainActivity.this, "It may take some time for the device to update IP address", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 400);
+                toast.show();
+
+                tv.setVisibility(View.INVISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+                String IP = getIP();
+                tv.setText(R.string.no_ip);
+                if (IP != null) {
+                    if (IP.length() > 0) {
+                        tv.setText(IP);
+                    }
+                }
+                tv.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
     }
 }
